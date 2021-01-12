302572e9332bcc4f2de0729856590db7dcd47fc8
ArturTrautenmuller <39464553+ArturTrautenmuller@users.noreply.github.com>
2021-01-11 21:02:14 -0300

    v.1.0.0

---
+++ IntegerCalculator/Integer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Linq;
+
+namespace IntegerCalculator
+{
+    public static class Integer
+    {
+        public static double Solve(string exp,List<string> variables, List<double> begin, List<double> end, double step)
+        {
+            double sum = 0;
+            if(variables.Count == 1)
+            {
+                for(double i = begin[0]; i <= end[0]; i += step)
+                {
+                    string vExp = exp.Replace(variables[0],i.ToString().Replace(",","."));
+                    sum += Convert.ToDouble(new DataTable().Compute(vExp, null)) * step;
+                }
+
+                return sum;
+            }
+
+            else
+            {
+                List<string> nVariables = RemoveFirst(variables.Cast<object>().ToList()).Cast<string>().ToList();
+                List<double> nBegin = RemoveFirst(begin.Cast<object>().ToList()).Cast<double>().ToList();
+                List<double> nEnd = RemoveFirst(end.Cast<object>().ToList()).Cast<double>().ToList();
+
+                for (double i = begin[0]; i <= end[0]; i += step)
+                {
+                    string vExp = exp.Replace(variables[0], i.ToString().Replace(",","."));
+                    sum += Solve(vExp,nVariables ,nBegin,nEnd,step) * step;
+                }
+
+                return sum;
+
+            }
+
+
+
+            
+        }
+
+        public static List<object> RemoveFirst(List<object> list)
+        {
+            List<object> newList = new List<object>();
+            for(int i = 1; i < list.Count;i++)
+            {
+                newList.Add(list[i]);
+            }
+
+            return newList;
+        }
+    }
+}
---
+++ IntegerCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntegerCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string expression = "2*x + 4*y + 5";
+            List<string> variables = new List<string>() { new string("y"),new string("x")};
+            List<double> begin = new List<double>() { 0,0 };
+            List<double> end = new List<double>() { 5,6 };
+            double step = 0.01;
+
+            double result = Integer.Solve(expression,variables,begin,end,step);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
